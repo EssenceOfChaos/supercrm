717172cfbdc0fce68ce87f907592e4b66d62449c
EssenceOfChaos <fjschiller@gmail.com>
2017-01-25 18:43:17 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root 'pages#index'
+  get 'pages/index'
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  get 'pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
