badbcd1c3e3a77f38b8ca9e135a1291a9bf4d86a
EssenceOfChaos <fjschiller@gmail.com>
2017-01-26 02:05:35 -0500

    dev commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   include UsersHelper
-helper_method :current_time
+helper_method :current_time, :date
+
+
+
+def date
+	date = Date.civil(params[:event]["date(1i)"].to_i,params[:event]["date(2i)"].to_i,params[:event]["date(3i)"].to_i)
+end
+
 
 
 def current_time
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,10 +5,13 @@ class Task
   field :title, type: String
   field :description, type: String
   field :priority, type: Integer
-  field :due, type: DateTime, default: tomorrow
-  field :user, type: String, default: current_user
+  field :due, type: DateTime
+  field :user, type: String, default: @user
+
+def date
+	date = Date.civil(params[:event]["date(1i)"].to_i,params[:event]["date(2i)"].to_i,params[:event]["date(3i)"].to_i)
+end
 
 
-
-  scope :tomorrow, -> DateTime.now.tomorrow.to_date
+  # scope :tomorrow, -> DateTime.now.tomorrow.to_date
 end
