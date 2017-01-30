47c466d4acf8e6fedb74c0c15b13631a3fb331fc
EssenceOfChaos <fjschiller@gmail.com>
2017-01-29 23:51:14 -0500

    working app in development mode

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task
   include Mongoid::Document
   include Mongoid::Timestamps
-<<<<<<< HEAD
   include Mongoid::Attributes::Dynamic
 
   field :title, type: String
@@ -10,15 +9,14 @@ class Task
   field :due, type: DateTime, default: Time.now
   field :user, type: String, default: @user
 
-=======
+
  before_validation :parse_date_if_not_null
 
   field :title, type: String
   field :description, type: String
   field :priority, type: Integer, default: '5'
-  field :due, type: Date, default: ->{ Date.today }
+  field :due, type: DateTime, default: ->{ Time.now }
   field :user, type: String, default: @user
-
 
 attr_accessor :date_string
 
@@ -27,8 +25,5 @@ def parse_date_if_not_null
       self.date = Date.strptime self.date_string, '%m/%d/%Y'
     end
   end
->>>>>>> ae29ce88347e53e4ca22f2f5008a93b0eec9a5b6
 
-
-  # scope :tomorrow, -> DateTime.now.tomorrow.to_date
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'pages/index'
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   get 'pages/contact'
+  get 'pages/index'
+  get 'pages/settings'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
